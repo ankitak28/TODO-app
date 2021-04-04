763de7814c88caef9b80dc2265db16b07c2c59da
Ankita Kumari <Ankitak28@gmail.com>
2021-04-04 11:33:28 +0530

    Project created and layout files added

---
+++ app/src/main/java/com/example/todo/Model/DataModel.java
@@ -0,0 +1,30 @@
+package com.example.todo.Model;
+
+public class DataModel {
+    private String task;
+    private int status,id;
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ app/src/main/java/com/example/todo/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static final int SPLASH_SCREEN_TIMEOUT = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }, SPLASH_SCREEN_TIMEOUT);
+    }
+}
